ddee524133dcdd1412a4ea42de1cef538d728d13
vstuen <vstuen@users.noreply.github.com>
2014-09-05 14:08:43 +0200

    BatWash.java 1
    Bare kommentarer om videre utførelse

---
+++ BatWash.java
@@ -0,0 +1,14 @@
+class BatWash {
+	public static int black;
+	public static void main(String[] args){
+
+		/** Set black value for reference by behaviour class ReactBlack */
+
+		/** Behaviour from LejOS API */
+
+		/** Arbitrator from LejOS API */
+
+		/** Run program */
+		
+	}
+}
